1dfdd2d925c9f45653f1e33838d32bb1e3880f59
Randall Gonzalez <randy1002@gmail.com>
2015-11-04 12:03:22 -0600

    Trying to deploy.

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/expedienteestudiantil',
+	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://172.24.28.21:27017/expedienteestudiantil-dev',
 	assets: {
 		lib: {
 			css: [
